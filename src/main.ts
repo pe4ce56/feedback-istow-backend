d57e34c95039e7ea63a95d5f4ff38cc5ac7f93b0
Damai Deo Saputro <51025335+pe4ce56@users.noreply.github.com>
2023-10-23 12:35:06 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as fs from 'fs'
+import * as fs from 'fs';
 async function bootstrap() {
 
   const httpsOptions = {
